3205535c8f98ea02c6be8ddc2ad6a66a27804205
Max Borzov <75090981+Borzow@users.noreply.github.com>
2022-09-27 22:25:02 +0300

    successfulAuthorization готов

--- src/main/java/tests/authorization/AuthorizationPage.java
+++ src/main/java/tests/authorization/AuthorizationPage.java
@@ -5,18 +5,20 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class AuthorizationPage {//todo оптимизировать Xpath
+public class AuthorizationPage {//todo оптимизировать Xpath , (String email,String password)- передача параметров так или картежом  из xml(например)
+    final String password = "nZhu)abOT2O3"; // todo спрятать лог/пас
+    final String email = "automation1testing@mail.ru";
     WebDriver driver;
     WebDriverWait wait;
     public AuthorizationPage(WebDriver driver,WebDriverWait wait){
         this.driver = driver;
         this.wait = wait;
     }
-    public void setEmail(String email){
+    public void setEmail(){
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name=\"email\"]")));
         driver.findElement(By.xpath("//input[@name=\"email\"]")).sendKeys(email);
     }
-    public void setPass(String password){
+    public void setPass(){
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name=\"password\"]")));
         driver.findElement(By.xpath("//input[@name=\"password\"]")).sendKeys(password);
     }
--- src/main/java/tests/authorization/FirstTest.java
+++ src/main/java/tests/authorization/FirstTest.java
@@ -5,19 +5,18 @@ import tests.WebDriverSettings;
 
 
 public class FirstTest extends WebDriverSettings {
-    final String pass = "nZhu)abOT2O3"; // todo спрятать лог/пас
-    final String log = "automation1testing@mail.ru";
+
     @Test
-    public void firstTest() {
+    public void successfulAuthorization() {
         //stepOne // Заходим на сайт kaspersky -> переходим на окно авторизации/регистрации
         HomePage homePage = new HomePage(driver, wait);
         AuthorizationPage authorizationPage = new AuthorizationPage(driver,wait);
         homePage.start();
         homePage.dropBox();
         homePage.MyKButton();
-        //stepTwo
-        authorizationPage.setEmail(log);
-        authorizationPage.setPass(pass);
+        //stepTwo Авторезируемся в аккаунт
+        authorizationPage.setEmail();
+        authorizationPage.setPass();
         authorizationPage.logInButton();
     }
 }
